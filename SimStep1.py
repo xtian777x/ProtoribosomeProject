853aff04e666c8d143efc9ec5cef380802aca9d2
christianp <christianp@wolfram.com>
2015-08-17 22:58:07 -0500

    Added test for distances

--- SimStep1.py
+++ SimStep1.py
@@ -9,14 +9,15 @@ import itertools
 baseProteins = {1:[(10, 10), (10, 12), (15, 12), (14, 11)], 2:[(14, 11), (15, 12), (16, 12), (16, 9), (13.5,9)], 3:[(13.5, 9), (16, 9), (16, 6), (13.8, 8)], 4:[(13.8, 8), (16, 6), (7, 6), (7, 7), (12, 7.2)], 5:[(7, 7), (12, 7.2), (10, 10), (10, 12), (7, 12)]}
 
 # 10 is the number of proteins and should be an input parameter
-bubble1 = [prBubble.Protein(baseProteins, random.randint(1,5)) for i in range(10)]
+bubble1 = [prBubble.Protein(baseProteins, random.randint(1,5), i, 1) for i in range(10)]
 
 #testing distances of all against all
-for a,b in itertools.combinations(bubble1,2):
-	print a.dist(b)
+for a,b in itertools.combinations(bubble1,2):	
+	if a.dist(b) < 100:
+		print a.id + " <-> " + b.id
 
-print "Original coordinates:\n"
-print bubble1[0].coords
-bubble1[0].move((10,-10))
-print "New coordinates:\n"
-print bubble1[0].coords
+#print "Original coordinates:\n"
+#print bubble1[0].coords
+#bubble1[0].move((10,-10))
+#print "New coordinates:\n"
+#print bubble1[0].coords
