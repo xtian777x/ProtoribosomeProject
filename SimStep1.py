abebc822e29d070ed8dcd59e4ba5494ebc08baa8
XTIAN <cpasquel777@gmail.com>
2016-02-11 00:54:07 -0500

    added basic merging of proteins

--- SimStep1.py
+++ SimStep1.py
@@ -7,6 +7,7 @@ import itertools
 import json
 import os
 import re
+from shapely.geometry import Polygon
 from shapely.ops import cascaded_union
 
 
@@ -14,7 +15,7 @@ from shapely.ops import cascaded_union
 baseProteins = {1:[(10, 10), (10, 12), (15, 12), (14, 11)], 2:[(14, 11), (15, 12), (16, 12), (16, 9), (13.5,9)], 3:[(13.5, 9), (16, 9), (16, 6), (13.8, 8)], 4:[(13.8, 8), (16, 6), (7, 6), (7, 7), (12, 7.2)], 5:[(7, 7), (12, 7.2), (10, 10), (10, 12), (7, 12)]}
 
 #matchingTypes contains, for each protein type, the types it can match to merge and form a new protein
-matchingTypes = {1:[2,5], 2:[1,3], 3:[2,4], 4:[3,5], 5:[4,1]}
+matchingTypes = {1:[2,5], 2:[1,3], 3:[2,4], 4:[3,5], 5:[4,1], 7:[]}
 
 #TODO: 100 is the number of proteins and should be an input parameter
 bubble1 = [prBubble.Protein(baseProteins, random.randint(1,5), i, 1) for i in range(100)]
@@ -31,8 +32,8 @@ for i in range(200):
 	# move each protein	
 	for protein in bubble1:		
 		#TODO: The movement max values should be an input parameter
-		protein.move((random.randint(-10,10), random.randint(-10,10)))
-		jdata.append({'id':protein.id, 'coords':protein.coords})
+		protein.move((random.randint(-1,1), random.randint(-1,1)))
+		jdata.append({'id':protein.id, 'coords':protein.coords,'status':protein.status})
 	
 	jdump['data'] = jdata
 	jdump_string = json.dumps(jdump, sort_keys=True, indent=1) #Human readable
@@ -41,23 +42,50 @@ for i in range(200):
 
 	# nearby proteins interacting
 	print "Interactions"
-	for protA,protB in itertools.combinations(bubble1,2):	
-		#TODO: This proximity threshold should be an input parameter
-		if protA.dist(protB) < 50: 
-			if protB.type in matchingTypes[protA.type]:
-				#Get the ID of each protein
-				protAIndex = int(re.search('P([0-9]+)$',protA.id).group(1))
-				protBIndex = int(re.search('P([0-9]+)$',protB.id).group(1))
+	for protA,protB in itertools.combinations(bubble1,2):
+		#Checks if both proteins are active
+		if protA.status == 1 and protB.status == 1:
+			#TODO: This proximity threshold should be an input parameter
+			if protA.dist(protB) < 5: 
+				#Checks if proteins can match
+				print "protA type: " + str(protA.type)
+				print "protB type: " + str(protB.type)
+				if protB.type in matchingTypes[protA.type]:	
+					#Checks if template is simple or compound
+					if protA.baseTemplate == 0:
+						protATemplate = baseProteins[protA.type]
+					else:
+						protATemplate = protA.baseTemplate
 
-				#Create a new protein from the base proteins using protA and protB types
-				newBaseProtein = list(cascaded_union([Polygon(baseProteins[protA.type]), Polygon(baseProteins[protB.type])]).exterior.coords)
+					if protB.baseTemplate == 0:
+						protBTemplate = baseProteins[protB.type]
+					else:
+						protBTemplate = protB.baseTemplate
 
-				#Compute the movement vector to position new base protein in the correct coordinates
-				movementVector = (protB.coords[0][0]-baseProteins[protB.type][0][0], protB.coords[0][1]-baseProteins[protB.type][0][1])
+					#Compute the movement vector to position new base protein in the correct coordinates
+					movementVector = (protB.coords[0][0]-protBTemplate[0][0], protB.coords[0][1]-protBTemplate[0][1])	
 
-				
+					#Create a new protein from the base protein templates
+					newBaseProtein = list(cascaded_union([Polygon(protATemplate), Polygon(protBTemplate)]).exterior.coords)
 
-				print protA.id + " (type " +  str(protA.type) + ")" + " <-> " + protB.id + " (type " +  str(protB.type) + ")"
+					##Update proteins##
+					#Save new base template
+					protB.baseTemplate = newBaseProtein
+					#Deactivate protein A
+					protA.status = 0
+					#Change protein B nature
+					protB.nature = "H"
+					#Update protein B ID
+					protB.id = protB.id + "-" + protA.id
+					#Update type
+					#protB.type = [protB.type, protA.type]
+					protB.type = 7
+					#Update coordinates
+					protB.coords = newBaseProtein
+					#Move to correct position
+					protB.move(movementVector)			
+
+					#print protA.id + " (type " +  str(protA.type) + ")" + " <-> " + protB.id + " (type " +  str(protB.type) + ")"
 jfile.seek(-1, os.SEEK_END)
 jfile.truncate()
 jfile.write(']')
